643c49570b98ba77f2c41d41ee61a6e0f3134f34
윤태성 <masonyun@yuntaeseongs-MacBook-Pro.local>
2024-06-08 07:57:22 +0900

    init commit

---
+++ src/pages/api/upload.ts
@@ -0,0 +1,48 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import type { NextApiRequest, NextApiResponse } from "next";
+import { createRouter } from "next-connect";
+import multer from "multer";
+
+interface MulterRequest extends NextApiRequest {
+  file: Express.Multer.File;
+}
+
+const router = createRouter<MulterRequest, NextApiResponse>();
+
+const upload = multer({
+  storage: multer.memoryStorage(),
+  limits: {
+    fileSize: 500 * 1024 * 1024, // 500MB
+  },
+}).single("file");
+
+router.use((req, res, next) => {
+  console.log("Request received");
+  next();
+});
+
+router.use(upload as any);
+
+router.post(async (req, res) => {
+  const originalFile = req.file;
+
+  res.status(200).json({ name: "John Doe" });
+});
+
+export const config = {
+  api: {
+    bodyParser: false,
+  },
+};
+
+export default router.handler({
+  onError: (err, req, res) => {
+    if (err instanceof multer.MulterError) {
+      console.error("Multer error:", err.stack);
+      res.status(500).end(`Multer error: ${err.message}`);
+    } else {
+      console.error("Unexpected error:", err);
+      res.status(500).end("An unexpected error occurred");
+    }
+  },
+});
